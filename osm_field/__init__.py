9ae05d639e90fb6c2273a79dd1817b6a1ae3996d
Markus Holtermann <info@markusholtermann.eu>
2017-02-06 21:51:23 +0100

    Bump version: 0.3.0 → 0.3.1

--- osm_field/__init__.py
+++ osm_field/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Markus Holtermann, et al'
 __email__ = 'info@markusholtermann.eu'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 default_app_config = 'osm_field.apps.OSMFieldConfig'
